8120ee2a94a6863f5ae2191423b9eb94df6b8745
Devid Fan <cptdavid@yandex.ru>
2023-03-12 13:39:40 +0300

    Проектная работа 4 Проверка 3

--- scripts/script.js
+++ scripts/script.js
@@ -5,40 +5,33 @@ let editButton = page.querySelector('.profile__edit');
 let closeButton = page.querySelector('.popup__close-button');
 let authorName = document.querySelector('.profile__author');
 let authorAbout = document.querySelector('.profile__about');
-let nameInput = document.querySelector('.popup__input_author-name');
-let aboutInput = document.querySelector('.popup__input_author-about');
+let nameInput = document.querySelector('input[name="nameInput"]');
+let aboutInput = document.querySelector('input[name="aboutInput"]');
 let form = page.querySelector('.popup__form');
 // let likeButtonOn = page.querySelector('.elements__like-button_disabled');
 
-function openPopup() {
+function editProfile() {
   popup.classList.add('popup_opened');
+  nameInput.value = authorName.textContent;
+  aboutInput.value = authorAbout.textContent;
 }
+
+editButton.addEventListener('click', editProfile);
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', openPopup);
-
-
 closeButton.addEventListener('click', closePopup);
-
-function editProfile() {
-
-  nameInput.value = authorName.textContent;
-  aboutInput.value = authorAbout.textContent;
-
-}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   authorName.textContent = nameInput.value;
   authorAbout.textContent = aboutInput.value;
-  
+
   closePopup();
 
 })
-editProfile();
 
 // function likeOn () {
 //   let likeButtonOn = page.querySelector('.elements__like-button_disabled');
