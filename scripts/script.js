022cc047624cf64583f05bb2158d4fc758837591
Devid Fan <cptdavid@yandex.ru>
2023-03-26 14:03:53 +0300

    Проектная работа 5 проверка 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,26 @@
-let page = document.querySelector('.page');
-let popup = page.querySelector('.popup');
-let closeButtons = page.querySelectorAll('.popup__close-button');
+const page = document.querySelector('.page');
+const popupGeneral = page.querySelector('.popup');
+const closeButtons = page.querySelectorAll('.popup__close-button');
 const popups = page.querySelectorAll('.popup');
 
-let profileInformation = page.querySelector('.profile__information');
-let editButton = page.querySelector('.profile__edit');
-let authorName = document.querySelector('.profile__author');
-let authorAbout = document.querySelector('.profile__about');
-let nameInput = document.querySelector('input[name="nameInput"]');
-let aboutInput = document.querySelector('input[name="aboutInput"]');
+const profileInformation = page.querySelector('.profile__information');
+const editButton = page.querySelector('.profile__edit');
+const authorName = document.querySelector('.profile__author');
+const authorAbout = document.querySelector('.profile__about');
+const nameInput = document.querySelector('input[name="nameInput"]');
+const aboutInput = document.querySelector('input[name="aboutInput"]');
 const profileEditForm = document.forms.editProfile;
 const imageProfileForm = document.forms.imageProfile;
 const imageEnlargePopup = page.querySelector('.popup_type_enlarge');
 const popupEnlarge = imageEnlargePopup.querySelector('.popup__image');
 const popupTitle = imageEnlargePopup.querySelector('.popup__title');
-
-const editImage = page.querySelector('.profile__add');
+const profileEdit = page.querySelector('.popup_type_profile');
+const imageEdit = page.querySelector('.profile__add');
 const imageAddPopup = page.querySelector('.popup_type_images')
 const imageAddPopupButton = imageAddPopup.querySelector('.popup__submit');
-
+// const profileClose = page.querySelector('.popup__close-button_type_profile');
+// const imagesClose = page.querySelector('.popup__close-button_type_images'); 
+// const enlargeClose = page.querySelector('.popup__close-button_type_enlarge'); 
 const titleInput = document.querySelector('input[name="titleInput"]');
 const srcInput = document.querySelector('input[name="srcInput"]');
 
@@ -53,31 +55,36 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+// Открытие попап
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+} 
+
 
 // Это поп ап профиля
 function editProfile() {
-  popup.classList.add('popup_opened');
+  openPopup(profileEdit);
   nameInput.value = authorName.textContent;
   aboutInput.value = authorAbout.textContent;
+ 
 }
 
 
 
-function closeAllPopups() {
-  popups.forEach((item) => {
-    item.classList.remove('popup_opened');
-  })
-}
+function closePopup(popup) {
+
+  popup.classList.remove('popup_opened');
+  }
+
 
 
 function editImages() {
-  imageAddPopup.classList.add('popup_opened');
-
+  openPopup(imageAddPopup);
 }
 
 
 // добавление картинок
-function addImage(argumentOne, argumentTwo) {
+function addImage(cardData) {
   const cardElement = elementTemplate.cloneNode(true);
 
   const elementTitle = cardElement.querySelector('.elements__title');
@@ -86,34 +93,47 @@ function addImage(argumentOne, argumentTwo) {
 // увеличить картинки, передаем из темплейта в попап   
   elementImage.addEventListener('click', () => {
     
-    imageEnlargePopup.classList.add('popup_opened');
+    openPopup(imageEnlargePopup);
     popupTitle.textContent = elementTitle.textContent;
    
     popupEnlarge.src = elementImage.src;
+    popupEnlarge.alt = cardData.name;
   });
 
   
 
-  elementTitle.textContent = argumentOne;
-  elementImage.src = argumentTwo;
+  elementTitle.textContent = cardData.name;
+  elementImage.src = cardData.link;
+  elementImage.alt = cardData.name;
 
   const buttonLike = cardElement.querySelector('.elements__like-button');
   const buttonDelete = cardElement.querySelector('.elements__delete');
 
-  buttonDelete.addEventListener('click', removeImage);
+  buttonDelete.addEventListener('click', () => {
+    const parent = buttonDelete.closest('.elements__item');
+    parent.remove();
+  });
   
   buttonLike.addEventListener('click', () => {
     toggleLike(buttonLike);
   });
   
+  return cardElement;
+}
+
+function renderCard(cardElement){
+
   elementsList.prepend(cardElement);
 }
+
+
+
 // Удалить карточку через родителя
-function removeImage () {
-  const child = document.querySelector('.elements__delete');
-  const parent = child.closest('.elements__item');
-  parent.remove()
-}
+// function removeImage () {
+//   const child = document.querySelector('.elements__delete');
+//   const parent = child.closest('.elements__item');
+//   parent.remove();
+// } 
 
 
 // Лайки активируются
@@ -123,46 +143,82 @@ function toggleLike(button){
 
 }
 
-closeButtons.forEach((item) => {
-  item.addEventListener('click', closeAllPopups);
+// profileClose.addEventListener('click',() => {
+//   closePopup(profileEdit);
+// })
 
-}
-)
+// imagesClose.addEventListener('click',() => {
+//   closePopup(imageAddPopup);
+// })
+
+// enlargeClose.addEventListener('click',() => {
+//   closePopup(imageEnlargePopup);
+// })
+
+
 
 profileEditForm.addEventListener('submit', (e) => {
   e.preventDefault();
   authorName.textContent = nameInput.value;
   authorAbout.textContent = aboutInput.value;
 
-  closeAllPopups();
+  closePopup(profileEdit);
 
 })
 
 imageProfileForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  const card =  addImage({
+    name: titleInput.value,
+    link: srcInput.value
+   });
 
-  closeAllPopups();
+  
+
+   renderCard(card);
+  closePopup(imageAddPopup);
 
 })
 
 // Использовал универсальную функцию, нужно повторить функции 
 
-initialCards.forEach(function (element) {
- addImage(element.name, element.link);
-    
+initialCards.forEach(function (obj) {
+   const card = addImage(obj);
 
+renderCard(card);
 
 })
 
+closeButtons.forEach(button => {
+  const buttonsPopup = button.closest('.popup'); // нашли родителя с нужным классом
+  button.addEventListener('click', () => closePopup(buttonsPopup)); // закрыли попап
+});  
+
+
 editButton.addEventListener('click', editProfile);
 
-editImage.addEventListener('click', editImages);
-
-imageAddPopupButton.addEventListener('click', () => {
-  addImage(titleInput.value, srcInput.value);
-});
+imageEdit.addEventListener('click', editImages);
 
 
+const obj = {
+  name: 'Девид',
+  age: 28,
+  surname: 'Фан'
+}
+console.log(obj.name)
+
+
+function dance(a, b){
+
+  const cardElement = a + b;
+
+  return cardElement;
+
+}
+
+const c = dance(2, 2);
+
+console.log(c)
 
 
 
